5dac852d6f6dd642519a07b305a30b988fc3af4d
Steven-Dev <107848175+Stv-devl@users.noreply.github.com>
2022-10-09 10:09:14 +0700

    Add files via upload

--- Js/index.js
+++ Js/index.js
@@ -1,5 +1,11 @@
-// Submit event
+//Variables
+const input = document.querySelector("input");
+const form = document.querySelector("form");
+
+//Variable for get user informations display
 const logoContainer = document.querySelector(".logo-container");
+const profileName = document.querySelector(".profile-name");
+const profileJoined = document.querySelector(".profile-joined");
 const profileLogin = document.querySelector(".profile-login");
 const profileBio = document.querySelector(".profile-bio");
 const profileRepos = document.querySelector(".profile-repos");
@@ -9,6 +15,25 @@ const profileCity = document.querySelector(".profile-city");
 const profileTwitter = document.querySelector(".profile-twitter");
 const profileWebsite = document.querySelector(".profile-website");
 const profileGithub = document.querySelector(".profile-github");
+let months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aou",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log(input.value);
+});
 
 //function getApi=> get api with fetch and display
 function getApi() {
@@ -16,12 +41,18 @@ function getApi() {
     .then((responce) => responce.json())
     .then((responce) => {
       const data = responce;
-      console.log(data);
+      /*console.log(data);*/
 
+      //variables for set joined date
+      let dateChanger = data.created_at.split("T").shift().split("-");
+      let trueMonth = dateChanger[1] - 1;
+
+      //display profiles informations
       logoContainer.style.backgroundImage =
         "url(https://avatars.githubusercontent.com/u/583231?v=4)";
-
-      profileLogin.textContent = data.login;
+      profileName.textContent = data.name;
+      profileJoined.textContent = `Joined ${dateChanger[2]} ${months[trueMonth]} ${dateChanger[0]}`;
+      profileLogin.textContent = `@ ${data.login}`;
       profileBio.textContent = data.bio;
       profileRepos.textContent = data.public_repos;
       profileFollowers.textContent = data.followers;
@@ -32,4 +63,3 @@ function getApi() {
       profileGithub.textContent = data.company;
     });
 }
-getApi();
